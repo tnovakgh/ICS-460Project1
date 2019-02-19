924a0cfaf4cc5988e05734d5fa32055ca849fac1
Andrew <Andrew-S90@users.noreply.github.com>
2019-02-19 16:46:26 -0600

    Comments + deleted unused method

--- Sender/src/UDPSender.java
+++ Sender/src/UDPSender.java
@@ -3,6 +3,14 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+/**
+ * Sender for a passed binary file split up in chunks.
+ *   
+ * ICS460 - Group Project #1
+ * 
+ * @author Michael, Andrew, Troy (Team 5)
+ * 
+ */
 public class UDPSender {
 
     private DatagramSocket socket;
@@ -10,6 +18,9 @@ public class UDPSender {
     private static final int BUFFER_SIZE = 16;
     private byte[] buffer;
 
+    /**
+	 * Public constructor that creates a new socket on the local network
+	 */
     public UDPSender() {
         try {
             socket = new DatagramSocket();
@@ -19,22 +30,12 @@ public class UDPSender {
         }
     }
 
-    public String sendEcho(String msg) {
-        String received = "";
-        
-        try {
-            buffer = msg.getBytes();
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
-            socket.send(packet);
-            packet = new DatagramPacket(buffer, buffer.length);
-            socket.receive(packet);
-            received = new String(packet.getData(), 0, packet.getLength());
-        }catch(IOException ex){
-            ex.printStackTrace();
-        }
-        return received;
-    }
-
+    /**
+	 * Sends the file one packet at a time over the specified socket and port
+	 * 
+	 * @param fileName
+	 * 			the path to the input file that we will be sending to the receiver
+	 */
     public void sendFile(String fileName) {
         int packets = 0;
         
@@ -42,28 +43,28 @@ public class UDPSender {
             InputStream inputStream = new BufferedInputStream(new FileInputStream(fileName));
             buffer = new byte[BUFFER_SIZE];
             
-            while (inputStream.read(buffer) != -1) {
+            while (inputStream.read(buffer) != -1) { //create a new packet with a specified size and send it through the socket
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
                 socket.send(packet);
                 System.out.println(String.format("[%d][%d][%d]", packets, packets * BUFFER_SIZE, packets * BUFFER_SIZE + buffer.length));
                 packets++;
             }
-            buffer = "end".getBytes();
+            buffer = "end".getBytes(); //we have sent everything, send a signal to the receiver that this is the ending packet
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
             socket.send(packet);
             socket.close();
-            inputStream.close();
+            inputStream.close(); //close everything up
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        if(args.length < 1){
+        if (args.length < 1) { //if there was no file provided via commandline provide an error and exit.
             System.out.println("Please provide an input file.\n");
             System.exit(0);
         }
         UDPSender client = new UDPSender();
-        client.sendFile(args[0]);
+        client.sendFile(args[0]); //send the passed in command line file
     }
 }
--- UDPReceiver.java
+++ UDPReceiver.java
@@ -2,26 +2,42 @@ import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
+/**
+ * Receiver for a passed binary file split up in chunks.
+ *   
+ * ICS460 - Group Project #1
+ * 
+ * @author Michael, Andrew, Troy (Team 5)
+ * 
+ */
+
 public class UDPReceiver extends Thread {
+	
     private DatagramSocket socket;
     private boolean running;
     private static final int BUFFER_SIZE = 16;
     private byte[] buffer = new byte[BUFFER_SIZE];
 
-    public UDPReceiver() {
+    /**
+	 * Public constructor that creates an open socket on port 4445
+	 */
+    public UDPReceiver() { 
         try {
             socket = new DatagramSocket(4445);
         }catch(IOException ex){
             ex.printStackTrace();
         }
     }
-
+    
+    /**
+	 * The thread that will constantly loop to read in new packets.
+	 */
     public void run() {
         int packets = 0;
         running = true;
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
 
-        while (running) {
+        while (running) { //loop until the end of the data is received
             try {
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet);
@@ -29,7 +45,7 @@ public class UDPReceiver extends Thread {
                     running = false;
                     continue;
                 }
-                byteArrayOutputStream.write(packet.getData());
+                byteArrayOutputStream.write(packet.getData()); //Print out the packet information that will match the senders information
                 System.out.println(String.format("[%d][%d][%d]", packets + 1, packets * BUFFER_SIZE, packets * BUFFER_SIZE + buffer.length));
                 packets++;
                 buffer = new byte[BUFFER_SIZE];
@@ -39,15 +55,18 @@ public class UDPReceiver extends Thread {
         }
         try {
             byte[] result = byteArrayOutputStream.toByteArray();
-            OutputStream outputStream = new BufferedOutputStream(new FileOutputStream("duck.jpg"));
+            OutputStream outputStream = new BufferedOutputStream(new FileOutputStream("image.jpg"));
             outputStream.write(result);
             outputStream.close();
-        }catch(IOException ex){
+        } catch(IOException ex) {
             ex.printStackTrace();
         }
         socket.close();
     }
 
+    /**
+	 * Starts our thread.
+	 */
     public static void main(String[] args) {
         new UDPReceiver().start();
     }
